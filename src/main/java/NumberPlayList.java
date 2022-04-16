02e981f41b77b8a7ddb1726b88c23626102dd26c
Sirishakopparaju <sirishakopparaju98@gmail.com>
2022-04-16 16:05:20 +0530

    added:mynumber uc1.4 using functions

--- src/main/java/NumberPlayList.java
+++ src/main/java/NumberPlayList.java
@@ -1,2 +1,45 @@
-package PACKAGE_NAME;public class NumberPlayList {
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+public class NumberPlayList {
+    public static void main(String[] args) {
+        List<Integer> myNumberList = new ArrayList<Integer>();
+        for (int i = 0; i < 5; i++) myNumberList.add(i);
+        Iterator<Integer> it = myNumberList.iterator();
+        while (it.hasNext()) {
+            Integer i = it.next();
+            System.out.println("method 1:iterator value:" + i);
+        }
+
+        class MyConsumer implements Consumer<Integer> {
+            public void accept(Integer t) {
+                System.out.println("method 2 consumer implements:" + t);
+            }
+        }
+        MyConsumer action = new MyConsumer();
+        myNumberList.forEach(action);
+        myNumberList.forEach(new Consumer<Integer>() {
+            public void accept(Integer t) {
+                System.out.println("method 3 for each anonymous class value:" + t);
+
+            }
+        });
+
+        Consumer<Integer> myListAction = n -> {
+            System.out.println("method 4 for each lamda implements value::" + n);
+        };
+        myNumberList.forEach(myListAction);
+
+        myNumberList.forEach(n -> {
+            System.out.println("method 5 for each lamda value::" + n);
+
+        });
+        Function<Integer,Double> toDoubleFunction=Integer::doubleValue;
+        myNumberList.forEach(n->{
+            System.out.println("Method 6 for each lamda function using double value::"+toDoubleFunction.apply(n));
+        });
+    }
 }

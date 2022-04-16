04affb360dc3bc48a368d55793594a9ccaed58c0
Sirishakopparaju <sirishakopparaju98@gmail.com>
2022-04-16 09:22:14 +0530

    added arithematic operation program with static method using lamda expression

--- src/main/java/LambdaExpressionDemo.java
+++ src/main/java/LambdaExpressionDemo.java
@@ -2,6 +2,20 @@
 @FunctionalInterface
 interface ArithmeticOperation {
     public int calculate(int x,int y);
+    public static int addition(int x, int y) {
+        return x + y;
+    }
+        public static int multiply(int x,int y){
+            return x*y;
+
+        }
+
+    public static int divide(int x,int y){
+        return x/y;
+
+    }
+
+
 }
 public class LambdaExpressionDemo {
     public static void main(String[] args) {
@@ -11,6 +25,11 @@ public class LambdaExpressionDemo {
         System.out.println(multiply.calculate(6,5));
         System.out.println(addition.calculate(6,5));
         System.out.println(divide.calculate(16,4));
+        System.out.println(ArithmeticOperation.addition(6,7));
+        System.out.println(ArithmeticOperation.multiply(6,7));
+        System.out.println(ArithmeticOperation.divide(6,2));
+
+
     }
 
 }
